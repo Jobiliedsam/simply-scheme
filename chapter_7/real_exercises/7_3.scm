6da347c545b17f2d063f908862f4d0dccd8fda46
Jobiliedsam <jobiliedson@hotmail.com>
2022-06-30 00:42:06 -0300

    exercises(chapter_7): add Variables chapter real exercises solution

---
+++ chapter_7/real_exercises/7_3.scm
@@ -0,0 +1,5 @@
+;;; The following program doesn't work. Why not? Fix it.
+;;; (define (superlative adjective word)
+;;;   (se (word adjective 'est) word))
+(define (superlative adjective wd)
+  (se (word adjective 'est) wd))
